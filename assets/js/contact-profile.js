759d5a6d43b122d8dc0e41fd7a51e853b3e8fd9e
arturmatlega <artur@projets.pl>
2020-03-10 12:24:41 +0100

    js fix; add chatillo scss

--- assets/js/contact-profile.js
+++ assets/js/contact-profile.js
@@ -1,37 +1,41 @@
-const throttle = (func, limit) => {
-    let inThrottle;
-    return function() {
-        const args = arguments;
-        const context = this;
-        if (!inThrottle) {
-            func.apply(context, args);
-            inThrottle = true;
-            setTimeout(() => inThrottle = false, limit);
+const checkContactMobileBtn = document.querySelector("#contactMobileBtn");
+if (typeof(checkContactMobileBtn) != 'undefined' && checkContactMobileBtn != null) {
+
+    const throttle = (func, limit) => {
+        let inThrottle;
+        return function() {
+            const args = arguments;
+            const context = this;
+            if (!inThrottle) {
+                func.apply(context, args);
+                inThrottle = true;
+                setTimeout(() => inThrottle = false, limit);
+            }
         }
-    }
-};
+    };
 
-const  contactMobileButton = document.querySelector("#contactMobileBtn"),
-contactMobileContainer = document.querySelector("#contactMobileCtn");
+    const  contactMobileButton = document.querySelector("#contactMobileBtn"),
+    contactMobileContainer = document.querySelector("#contactMobileCtn");
 
-contactMobileBtn.addEventListener("click", function(e) {
-contactMobileCtn.classList.toggle("open");
-    e.stopPropagation();
-}, false);
+    contactMobileBtn.addEventListener("click", function(e) {
+    contactMobileCtn.classList.toggle("open");
+        e.stopPropagation();
+    }, false);
 
-document.addEventListener("click", function() {
-    contactMobileCtn.classList.remove("open");
-}, false);
-
-const  topLine = document.querySelector("#topLine");
-let lastScrollTop = 0;
-window.addEventListener("scroll", throttle(function(){ 
-    var st = window.pageYOffset ;
-    if (st > lastScrollTop){
-        topLine.classList.add("invisible");
-    } else {
-        topLine.classList.remove("invisible");
+    document.addEventListener("click", function() {
         contactMobileCtn.classList.remove("open");
-    }
-    lastScrollTop = st;
-},  100));
+    }, false);
+
+    const  topLine = document.querySelector("#topLine");
+    let lastScrollTop = 0;
+    window.addEventListener("scroll", throttle(function(){ 
+        var st = window.pageYOffset ;
+        if (st > lastScrollTop){
+            topLine.classList.add("invisible");
+        } else {
+            topLine.classList.remove("invisible");
+            contactMobileCtn.classList.remove("open");
+        }
+        lastScrollTop = st;
+    },  100));
+}
--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,7 +1,10 @@
-const menuButton = document.querySelector("#menuMobileBtn"),
-menuContainer = document.querySelector("#menuMobileCtn");
+const checkMenuMobileBtn = document.querySelector("#menuMobileBtn");
+if (typeof(checkMenuMobileBtn) != 'undefined' && checkMenuMobileBtn != null) {
+    const menuButton = document.querySelector("#menuMobileBtn"),
+    menuContainer = document.querySelector("#menuMobileCtn");
 
-menuButton.addEventListener("click", function() {
-    menuButton.classList.toggle("open");
-    menuContainer.classList.toggle("open");
-}, false);    
+    menuButton.addEventListener("click", function() {
+        menuButton.classList.toggle("open");
+        menuContainer.classList.toggle("open");
+    }, false);    
+}
